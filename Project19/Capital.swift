edbfc40057a3988edf0fe3bd1a16f508849d505c
halloween-jack <halloween.jack.dev@gmail.com>
2017-10-24 06:39:23 +0900

    Up and running with MapKit

---
+++ Project19/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project19
+//
+//  Created by slim on 2017/10/24.
+//  Copyright © 2017 halloween-jack. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
